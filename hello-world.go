0947c3064dbafc4455fcba40f3544bf6c6e42d85
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-09 17:09:38 -0500

    feat: use struct

--- hello-world.go
+++ hello-world.go
@@ -3,7 +3,21 @@ package main
 // For print
 import "fmt"
 
+type Video struct {
+	name        string
+	user        string
+	description string
+}
+
 // go fmt hello-world.go format file
 func main() {
 	fmt.Println("Hello World from GO")
+
+	// is mandatory '}' in the last property
+	var video = Video{
+		name:        "Video 0",
+		user:        "Woz",
+		description: "Description 0"}
+
+	fmt.Println(video)
 }
