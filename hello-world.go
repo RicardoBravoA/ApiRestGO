de85137dedbb82b6dff280e09a1a164f1f28cc9f
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-09 17:19:22 -0500

    feat: Use functions

--- hello-world.go
+++ hello-world.go
@@ -14,10 +14,15 @@ func main() {
 	fmt.Println("Hello World from GO")
 
 	// is mandatory '}' in the last property
-	var video = Video{
-		name:        "Video 0",
-		user:        "Woz",
-		description: "Description 0"}
+	var video = getVideo("User 0", "Woz", "Description 0")
 
 	fmt.Println(video)
+}
+
+func getVideo(name string, user string, description string) Video {
+	// is mandatory '}' in the last property
+	return Video{
+		name:        name,
+		user:        user,
+		description: description}
 }
