53321f8956b2599d2272c073982fe0e7d0f4765c
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-09 18:38:10 -0500

    feat(main): Add main function

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(request http.Request, response http.ResponseWriter){
+		fmt.Fprintf(response, "Hello world from my server with GO")
+	})
+}
