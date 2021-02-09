c7fce7eb2a8ac0963dfe5f4529e9c6665ba98f95
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-09 18:41:54 -0500

    feat(main): Show web server

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
 )
 
 func main() {
-	http.HandleFunc("/", func(request http.Request, response http.ResponseWriter){
+	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request,){
 		fmt.Fprintf(response, "Hello world from my server with GO")
 	})
+
+	server := http.ListenAndServe(":8080", nil)
+
+	log.Fatal(server)
 }
